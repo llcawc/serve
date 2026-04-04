a0155fe58092949b948188ad3db8f561148fd045
llcawc <hostmastername@gmail.com>
2026-04-04 16:47:35 +0300

    update vite.config.js

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,3 @@
-// @ts-check
 import '../styles/main.scss'
 import { schemeSwitcher } from './scheme'
 document.addEventListener('DOMContentLoaded', schemeSwitcher)
--- vite.config.js
+++ vite.config.js
@@ -7,22 +7,19 @@ const __dirname = cwd()
 export default defineConfig({
   root: join(__dirname, 'src'),
   publicDir: join(__dirname, 'public'),
-  server: {
-    port: 3000,
-  },
   build: {
     outDir: join(__dirname, 'dist'),
     emptyOutDir: true,
     manifest: false,
     assetsInlineLimit: 0,
     modulePreload: false,
-    rollupOptions: {
+    rolldownOptions: {
       input: ['./src/index.html', './src/404.html'],
       output: {
         entryFileNames: 'js/[name].js',
         chunkFileNames: 'js/[name].js',
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.names[0].split('.').pop()
+        assetFileNames: (chunkInfo) => {
+          let extType = chunkInfo.names[0].split('.').pop() ?? ''
           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
             extType = 'images'
           }
@@ -31,6 +28,16 @@ export default defineConfig({
           }
           return `${extType}/[name][extname]`
         },
+      },
+    },
+  },
+  server: {
+    port: 8080,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'color-functions', 'global-builtin', 'legacy-js-api', 'if-function'],
       },
     },
   },
